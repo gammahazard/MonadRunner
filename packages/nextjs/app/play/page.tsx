13057d53da2b8c948357ae55f59a5ebe1c9bda59
Morpheus42069 <morpheus69420@proton.me>
2025-03-11 17:19:33 -0400

    update yarn

--- packages/nextjs/app/play/page.tsx
+++ packages/nextjs/app/play/page.tsx
@@ -308,7 +308,7 @@ const Play: NextPage = () => {
                       <li>Move left - A</li>
                       <li>Move Right - B</li>
                       <li>Jump - W</li>
-                      <li>Stop Jump - S</li>
+                      <li>End Jump Early - S</li>
                     </ul>
                   </div>
                 </div>
